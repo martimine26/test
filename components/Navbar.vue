e4f387a87920b3c760d6f0db500f0ebdfa77077d
yaroslav <85133193+martimine26@users.noreply.github.com>
2021-06-02 21:59:26 +0300

    hi

---
+++ components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Nuxt SSR</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+         <nuxt-link exact active-class="active" class="nav-link" aria-current="page" to="/">
+         Home
+         </nuxt-link>
+        </li>
+        <li class="nav-item">
+         <nuxt-link active-class="active" class="nav-link" to="/about">
+         About
+         </nuxt-link>
+        </li>
+        <li class="nav-item">
+         <nuxt-link class="nav-link" to="/users">
+         Users
+         </nuxt-link>
+        </li>
+          <li class="nav-item">
+         <nuxt-link class="nav-link" to="/Login">
+         Login
+         </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <section>
+        <form>
+            <h1>Login pudge</h1>
+            <div class="form-group">
+                  <input type="text" class="form-contol">
+            </div>
+         
+           <p>
+               <nuxt-link to="/">Home</nuxt-link>
+           </p>
+
+
+
+        <button class="btn btn-primary" type="submit">
+          Login
+        </button>
+    
+        </form>
+    </section>
+</template>
+<script>
+export default {
+    layout: "empty"
+}
+</script>
+<style scoped>
+  input{
+      width: 400px;
+  }
+  form{
+      top: 500px;
+      width: 500px;
+      margin: auto 0; 
+  }
+</style>
